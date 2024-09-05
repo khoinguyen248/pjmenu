a4bc6e1a95f341e37bdb1626811ee94761c539b9
khoi <nguyenminhkhoi24082005@gmail.com>
2024-09-05 18:17:18 +0700

    change

--- tacotime.js
+++ tacotime.js
@@ -1,6 +1,14 @@
+let headas = document.querySelector('.heads')
+let nut = document.querySelector('.soc')
+let clicked = false
+function showing(){
+    clicked = !clicked
+    if(clicked){
+        headas.style.display = 'flex'
+    }
+    else{
+        headas.style.display = 'none'
+    }
+}
 
-$(document).ready(function(){
-    $('.image-slider').slick(
-       );
-  });
-              
+nut.addEventListener("click", showing)       
